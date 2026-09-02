03587d47be25d4de69053d8508d3bc327f51fdef
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:43:52 -0400

    Namespace Gorod Krovi navigation under Hub routes

--- maps/gorod-krovi/src/components/Header.jsx
+++ maps/gorod-krovi/src/components/Header.jsx
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import '../styles/Header.css';
 import banner from '../assets/GK_Banner.png';
 
+const BASE = '/maps/gorod-krovi';
+
 const Header = () => (
   <header className="header-container" style={{ backgroundImage: `url(${banner})` }}>
     <div className="header-overlay" />
@@ -12,11 +14,11 @@ const Header = () => (
         <p className="header-subtitle">Decode SOPHIA’s mission through the fires of Stalingrad.</p>
       </div>
       <nav className="header-nav">
-        <NavLink end to="/" className="nav-link">Main Guide</NavLink>
-        <NavLink to="/buildables" className="nav-link">Buildables</NavLink>
-        <NavLink to="/trophies" className="nav-link">Trophy Challenges</NavLink>
-        <NavLink to="/valves" className="nav-link">Valve Solver</NavLink>
-        <NavLink to="/boss" className="nav-link">Boss Fight</NavLink>
+        <NavLink end to={BASE} className="nav-link">Main Guide</NavLink>
+        <NavLink to={`${BASE}/buildables`} className="nav-link">Buildables</NavLink>
+        <NavLink to={`${BASE}/trophies`} className="nav-link">Trophy Challenges</NavLink>
+        <NavLink to={`${BASE}/valves`} className="nav-link">Valve Solver</NavLink>
+        <NavLink to={`${BASE}/boss`} className="nav-link">Boss Fight</NavLink>
       </nav>
     </div>
   </header>
