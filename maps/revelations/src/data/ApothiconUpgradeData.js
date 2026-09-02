e26434d00ad6097bb2a0fb20a7e78cfc26ed7c42
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 11:38:46 -0400

    Correct Revelations Apothicon upgrade area labels

--- maps/revelations/src/data/ApothiconUpgradeData.js
+++ maps/revelations/src/data/ApothiconUpgradeData.js
@@ -1,28 +1,26 @@
-// src/data/ApothiconUpgradeData.js
-
 const ApothiconUpgradeData = [
   {
-    name: "Spawn",
+    name: 'Spawn Corruption Engine',
     start: 125,
     end: 134,
   },
   {
-    name: "Shangri-La",
+    name: 'Der Eisendrache',
     start: 134,
     end: 141,
   },
   {
-    name: "Verruckt",
+    name: 'Verrückt / Shangri-La Stairs',
     start: 141,
     end: 144,
   },
   {
-    name: "Mob of the Dead",
+    name: 'Mob of the Dead Corruption Engine',
     start: 144,
     end: 147,
   },
   {
-    name: "Nacht der Untoten",
+    name: 'Nacht der Untoten',
     start: 146,
     end: 150,
   },
