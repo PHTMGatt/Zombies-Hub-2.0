f49c59d41eba671d392f6aaceb87a326922a474a
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:31:23 -0400

    Scaffold Origins route module

---
+++ maps/origins/routes/OriginsRoutes.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MigrationPlaceholder from '../../../shared/ui/MigrationPlaceholder';
+
+export default function OriginsRoutes() {
+  return <MigrationPlaceholder title="Origins">Origins source is being migrated and converted to nested Hub routes.</MigrationPlaceholder>;
+}
