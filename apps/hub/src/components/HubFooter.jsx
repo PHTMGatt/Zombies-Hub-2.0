643b2496a5cd0f695906299f932cf2de1dcc1fea
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:29:51 -0400

    Add shared Hub footer

---
+++ apps/hub/src/components/HubFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/hub-footer.css';
+
+export default function HubFooter() {
+  return (
+    <footer className="hub-footer">
+      <span>Zombies Hub 2.0</span>
+      <span>Unified Call of Duty Zombies companion</span>
+    </footer>
+  );
+}
