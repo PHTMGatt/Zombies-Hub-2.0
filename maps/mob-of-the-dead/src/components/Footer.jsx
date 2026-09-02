bf5a5375e9f6efbf1b71cc84e498d903e3aecc84
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 12:42:46 -0400

    Simplify Mob of the Dead footer

--- maps/mob-of-the-dead/src/components/Footer.jsx
+++ maps/mob-of-the-dead/src/components/Footer.jsx
@@ -4,16 +4,11 @@ import '../styles/Footer.css';
 const Footer = () => (
   <footer className="mob-footer">
     <div className="mob-footer__content">
-      <div className="mob-footer__identity">
-        <span className="mob-footer__kicker">Zombies Hub 2.0</span>
-        <strong>Mob of the Dead</strong>
-      </div>
-      <div className="mob-footer__divider" aria-hidden="true" />
-      <div className="mob-footer__meta">
-        <span>Pop Goes the Weasel</span>
-        <span aria-hidden="true">•</span>
-        <span>Alcatraz</span>
-      </div>
+      <strong>Mob of the Dead</strong>
+      <span aria-hidden="true">•</span>
+      <span>Pop Goes the Weasel</span>
+      <span aria-hidden="true">•</span>
+      <span>Alcatraz</span>
     </div>
   </footer>
 );
