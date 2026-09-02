dcd5421c68b78711960a15481ebf46afffee37ea
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 01:40:25 -0400

    Refocus Mob navigation on the actual Easter Egg run

--- maps/mob-of-the-dead/src/components/Nav.jsx
+++ maps/mob-of-the-dead/src/components/Nav.jsx
@@ -4,14 +4,13 @@ import { NavLink } from 'react-router-dom';
 const BASE = '/maps/mob-of-the-dead';
 
 const Nav = () => (
-  <nav className="nav">
+  <nav className="nav" aria-label="Mob of the Dead guide navigation">
     <ul>
-      <li><NavLink to={BASE} end>Overview</NavLink></li>
-      <li><NavLink to={`${BASE}/free-blundergat`}>Blundergat</NavLink></li>
-      <li><NavLink to={`${BASE}/spoon`}>Spoon</NavLink></li>
-      <li><NavLink to={`${BASE}/redeemer`}>Redeemer</NavLink></li>
-      <li><NavLink to={`${BASE}/logs`}>Logs</NavLink></li>
-      <li><NavLink to={`${BASE}/final-step`}>Final Step</NavLink></li>
+      <li><NavLink to={BASE} end>Guide</NavLink></li>
+      <li><NavLink to={`${BASE}/plane`}>Plane</NavLink></li>
+      <li><NavLink to={`${BASE}/retriever-skulls`}>Retriever + Skulls</NavLink></li>
+      <li><NavLink to={`${BASE}/codes`}>Codes</NavLink></li>
+      <li><NavLink to={`${BASE}/final-step`}>Final Fight</NavLink></li>
     </ul>
   </nav>
 );
