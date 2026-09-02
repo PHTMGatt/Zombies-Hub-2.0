e670d1d44952b204b74cfee8403e46fea523e44b
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:31:47 -0400

    Scaffold Revelations route module

---
+++ maps/revelations/routes/RevelationsRoutes.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MigrationPlaceholder from '../../../shared/ui/MigrationPlaceholder';
+
+export default function RevelationsRoutes() {
+  return <MigrationPlaceholder title="Revelations">Revelations source is being migrated and converted to nested Hub routes.</MigrationPlaceholder>;
+}
