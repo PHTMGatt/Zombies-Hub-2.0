759ed66aa4be1440c6373b3162c306c6b1132b0b
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:31:34 -0400

    Scaffold Der Eisendrache route module

---
+++ maps/der-eisendrache/routes/DerEisendracheRoutes.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MigrationPlaceholder from '../../../shared/ui/MigrationPlaceholder';
+
+export default function DerEisendracheRoutes() {
+  return <MigrationPlaceholder title="Der Eisendrache">Der Eisendrache source is being migrated and converted to nested Hub routes.</MigrationPlaceholder>;
+}
