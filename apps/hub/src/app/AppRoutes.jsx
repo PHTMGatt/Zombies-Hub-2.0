651fcf45da5fc2a753509327839adaa413dcdd22
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:29:36 -0400

    Add unified Hub and map route registry

---
+++ apps/hub/src/app/AppRoutes.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import AllMaps from '../pages/AllMaps';
+import EasterEggs from '../pages/EasterEggs';
+import MapInfo from '../pages/MapInfo';
+import SideEasterEggList from '../pages/SideEasterEggList';
+import SideEEInfo from '../pages/SideEEInfo';
+import OriginsRoutes from '../../../../maps/origins/routes/OriginsRoutes';
+import MobRoutes from '../../../../maps/mob-of-the-dead/routes/MobRoutes';
+import DerEisendracheRoutes from '../../../../maps/der-eisendrache/routes/DerEisendracheRoutes';
+import ZetsubouRoutes from '../../../../maps/zetsubou-no-shima/routes/ZetsubouRoutes';
+import GorodRoutes from '../../../../maps/gorod-krovi/routes/GorodRoutes';
+import RevelationsRoutes from '../../../../maps/revelations/routes/RevelationsRoutes';
+
+export default function AppRoutes({ RootLayout }) {
+  return (
+    <Routes>
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="allmaps" element={<AllMaps />} />
+        <Route path="easter-eggs" element={<EasterEggs />} />
+        <Route path="info/:slug" element={<MapInfo />} />
+        <Route path="side-easter-eggs" element={<SideEasterEggList />} />
+        <Route path="side-easter-eggs/:mapKey" element={<SideEEInfo />} />
+
+        <Route path="maps/origins/*" element={<OriginsRoutes />} />
+        <Route path="maps/mob-of-the-dead/*" element={<MobRoutes />} />
+        <Route path="maps/der-eisendrache/*" element={<DerEisendracheRoutes />} />
+        <Route path="maps/zetsubou-no-shima/*" element={<ZetsubouRoutes />} />
+        <Route path="maps/gorod-krovi/*" element={<GorodRoutes />} />
+        <Route path="maps/revelations/*" element={<RevelationsRoutes />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
+  );
+}
