c5233b13d0e3fe63d6e5d2279cac54dccf55b78b
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 01:27:12 -0400

    Tighten Mob header hierarchy

--- maps/mob-of-the-dead/src/components/Header.jsx
+++ maps/mob-of-the-dead/src/components/Header.jsx
@@ -1,11 +1,10 @@
-// src/components/Header.jsx
 import React from 'react';
 import Nav from './Nav';
 import bannerSrc from '../assets/images/mob-banner.png';
 import '../styles/Header.css';
 
 const Header = () => (
-  <header className="header">
+  <header className="header mob-header">
     <img
       src={bannerSrc}
       alt="Mob of the Dead banner"
@@ -13,11 +12,12 @@ const Header = () => (
     />
 
     <div className="header-content">
+      <span className="mob-header__kicker">Alcatraz · Zombies Hub 2.0</span>
       <h1>
-        Mob of the Dead<br />
+        Mob of the Dead
         <small>Pop Goes the Weasel</small>
       </h1>
-      <Nav /> {/* Note: Nav lives here now, removed from App.jsx */}
+      <Nav />
     </div>
   </header>
 );
