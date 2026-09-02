38300b8b70bdcdb75e3fd3ef33231820814eb7d5
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:31:43 -0400

    Scaffold Gorod Krovi route module

---
+++ maps/gorod-krovi/routes/GorodRoutes.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MigrationPlaceholder from '../../../shared/ui/MigrationPlaceholder';
+
+export default function GorodRoutes() {
+  return <MigrationPlaceholder title="Gorod Krovi">Gorod Krovi source is being migrated and converted to nested Hub routes.</MigrationPlaceholder>;
+}
