08d4a5fc66efb590087acc234500b6268e930d37
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 11:54:26 -0400

    Namespace Side Easter Egg content card classes

--- apps/hub/src/components/SideEECard.jsx
+++ apps/hub/src/components/SideEECard.jsx
@@ -1,17 +1,14 @@
-// File: SideEECard.jsx
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/SideEE/SideEECard.css';
 
 const SideEECard = ({ title, description }) => (
-  <div className="side-ee-card">
-    <h3 className="side-ee-card__title">{title}</h3>
-    <p className="side-ee-card__desc">{description}</p>
-  </div>
+  <article className="side-ee-entry-card">
+    <h3 className="side-ee-entry-card__title">{title}</h3>
+    <p className="side-ee-entry-card__desc">{description}</p>
+  </article>
 );
 
-// Prop validation
 SideEECard.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
