a7b4a1c5c1b9bec4bafe07951b534e06ca4587b0
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 13:38:57 -0400

    Simplify Shadows of Evil header

--- maps/shadows-of-evil/src/components/Header.jsx
+++ maps/shadows-of-evil/src/components/Header.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import soeCover from '../../../../apps/hub/src/assets/images/Maps/Shadows_Of_Evil.jpg';
 import '../styles/Shadows.css';
-
-const BASE = '/maps/shadows-of-evil';
 
 export default function Header() {
   return (
     <header className="soe-header">
-      <img className="soe-header__image" src={soeCover} alt="Shadows of Evil" />
+      <img className="soe-header__image" src={soeCover} alt="" aria-hidden="true" />
       <div className="soe-header__overlay" />
       <div className="soe-header__content">
         <div className="soe-header__identity">
-          <span className="soe-header__kicker">Morg City</span>
           <h1>Shadows of Evil</h1>
-          <p>Main Easter Egg Guide</p>
+          <p>Main Easter Egg</p>
         </div>
-        <nav aria-label="Shadows of Evil guide navigation">
-          <NavLink to={BASE} end>Guide</NavLink>
-        </nav>
       </div>
     </header>
   );
