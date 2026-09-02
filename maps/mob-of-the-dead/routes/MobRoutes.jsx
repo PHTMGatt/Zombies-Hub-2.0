c2564e7b65baaa62f88a9cc525e1e3f5b0e7f5b6
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:31:28 -0400

    Scaffold Mob of the Dead route module

---
+++ maps/mob-of-the-dead/routes/MobRoutes.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MigrationPlaceholder from '../../../shared/ui/MigrationPlaceholder';
+
+export default function MobRoutes() {
+  return <MigrationPlaceholder title="Mob of the Dead">Mob of the Dead source is being migrated and converted to nested Hub routes.</MigrationPlaceholder>;
+}
