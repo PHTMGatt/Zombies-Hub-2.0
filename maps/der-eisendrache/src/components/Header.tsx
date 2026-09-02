0c5bd7400a448b3ba25042416ef999549e5fd36c
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 03:28:38 -0400

    Redesign Der Eisendrache map header

--- maps/der-eisendrache/src/components/Header.tsx
+++ maps/der-eisendrache/src/components/Header.tsx
@@ -1,15 +1,19 @@
-// 'Header.tsx'
-// -------------
-
 import React from 'react';
 import NavBar from './NavBar';
 import '../styles/components/Header.css';
 
 const Header: React.FC = () => (
-  <header className="header-site">
-    <div className="header-site__bg" />
-    <div className="header-site__content">
-      <h1 className="header-site__title">Der Eisendrache EE Guide</h1>
+  <header className="de-header">
+    <div className="de-header__art" aria-hidden="true" />
+    <div className="de-header__veil" aria-hidden="true" />
+    <div className="de-header__content">
+      <div className="de-header__identity">
+        <span className="de-header__eyebrow">Black Ops III Zombies</span>
+        <div className="de-header__title-row">
+          <h1>Der Eisendrache</h1>
+          <span>My Brother’s Keeper</span>
+        </div>
+      </div>
       <NavBar />
     </div>
   </header>
