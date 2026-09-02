1fd346c3dfa657c6356a9a717cf258f42eab9346
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:47:19 -0400

    Namespace alternate Revelations navigation

--- maps/revelations/src/components/Nav.jsx
+++ maps/revelations/src/components/Nav.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
+const BASE = '/maps/revelations';
+
 function Nav() {
   return (
     <nav className="nav">
-      <Link to="/" className="nav-link">Main Guide</Link>
-      <Link to="/apothicon-upgrade" className="nav-link">Apothicon Upgrade</Link>
-      <Link to="/egg-locations" className="nav-link">Egg Locations</Link>
-      <Link to="/summoning-key" className="nav-link">Summoning Key</Link>
-      <Link to="/sound-step" className="nav-link">Sound Step</Link>
+      <Link to={BASE} className="nav-link">Main Guide</Link>
+      <Link to={`${BASE}/apothicon-upgrade`} className="nav-link">Apothicon Upgrade</Link>
+      <Link to={`${BASE}/egg-locations`} className="nav-link">Egg Locations</Link>
+      <Link to={`${BASE}/summoning-key`} className="nav-link">Summoning Key</Link>
+      <Link to={`${BASE}/sound-step`} className="nav-link">Sound Step</Link>
     </nav>
   );
 }
