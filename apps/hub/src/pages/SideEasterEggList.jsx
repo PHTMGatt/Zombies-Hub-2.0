0f6b9aad97600ac7ca7415822f686455e89ca84c
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:33:20 -0400

    Add temporary side Easter egg list

---
+++ apps/hub/src/pages/SideEasterEggList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SideEasterEggList() {
+  return (
+    <section className="hub-page">
+      <h1>Side Easter Eggs</h1>
+      <p>The existing Side Easter Egg data and cards are being migrated from the original Hub.</p>
+    </section>
+  );
+}
