4a49a82dd24c5f90787289e53e9526e75d826833
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 12:37:59 -0400

    Simplify Zetsubou footer

--- maps/zetsubou-no-shima/src/components/Footer.jsx
+++ maps/zetsubou-no-shima/src/components/Footer.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import '../styles/Footer.css';
 
-const Footer = () => {
-  return (
-    <footer className="zets-footer">
-      <div className="zets-footer__backdrop" aria-hidden="true" />
-      <div className="zets-footer__content">
-        <div className="zets-footer__identity">
-          <span className="zets-footer__kicker">Zombies Hub 2.0</span>
-          <strong>Zetsubou No Shima</strong>
-        </div>
-        <div className="zets-footer__divider" aria-hidden="true" />
-        <div className="zets-footer__meta">
-          <span>Seeds of Doubt</span>
-          <span className="zets-footer__dot" aria-hidden="true">•</span>
-          <span>Pohnpei</span>
-        </div>
-      </div>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="zets-footer">
+    <div className="zets-footer__backdrop" aria-hidden="true" />
+    <div className="zets-footer__content">
+      <strong>Zetsubou No Shima</strong>
+      <span aria-hidden="true">•</span>
+      <span>Seeds of Doubt</span>
+      <span aria-hidden="true">•</span>
+      <span>Pohnpei</span>
+    </div>
+  </footer>
+);
 
 export default Footer;
