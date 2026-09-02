06435942edb50ff76e6fbd690b8dbd2585addfce
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:31:39 -0400

    Scaffold Zetsubou route module

---
+++ maps/zetsubou-no-shima/routes/ZetsubouRoutes.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MigrationPlaceholder from '../../../shared/ui/MigrationPlaceholder';
+
+export default function ZetsubouRoutes() {
+  return <MigrationPlaceholder title="Zetsubou No Shima">Zetsubou source is being migrated and converted to nested Hub routes.</MigrationPlaceholder>;
+}
