96bc1a1dd01ebbe0ab2213e13898d3f4947b691c
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-02 12:27:21 -0400

    Compact Origins map header

--- maps/origins/src/components/Header.tsx
+++ maps/origins/src/components/Header.tsx
@@ -10,16 +10,10 @@ const Header = () => {
 
       <div className="origins-header__content">
         <div className="origins-header__identity">
-          <span className="origins-header__kicker">Zombies Hub 2.0 · Black Ops II</span>
-
-          <div className="origins-header__title-row">
-            <img src={ReviveIcon} alt="" aria-hidden="true" className="revive-header-icon" />
-            <div>
-              <h1 className="header-title">Origins</h1>
-              <p className="origins-header__subtitle">Little Lost Girl</p>
-            </div>
-            <img src={ReviveIcon} alt="" aria-hidden="true" className="revive-header-icon" />
-          </div>
+          <img src={ReviveIcon} alt="" aria-hidden="true" className="revive-header-icon" />
+          <h1 className="header-title">Origins</h1>
+          <span className="origins-header__subtitle">Little Lost Girl</span>
+          <img src={ReviveIcon} alt="" aria-hidden="true" className="revive-header-icon origins-header__end-icon" />
         </div>
 
         <div className="header-right">
