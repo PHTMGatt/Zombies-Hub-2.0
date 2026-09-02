d367646f1031d075b34dc5b764a6d0ac2739412f
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:35:42 -0400

    Point featured map registry at internal 2.0 routes

--- apps/hub/src/data/featuredMaps.js
+++ apps/hub/src/data/featuredMaps.js
@@ -1,5 +1,3 @@
-// src/data/featuredMaps.js
-
 import AllMapsImg        from '../assets/images/Featured/AllMaps.jpg';
 import OriginsImg        from '../assets/images/Featured/Origins.webp';
 import RevelationsImg    from '../assets/images/Featured/Rev.jpg';
@@ -9,48 +7,13 @@ import GorodKroviImg     from '../assets/images/Featured/GK.jpg';
 import SideEEImg         from '../assets/images/Featured/SideEE.jpg';
 
 const featuredMaps = [
-  {
-    name: 'All Maps',
-    renderLink: '/allmaps',
-    slug: null,
-    image: AllMapsImg,
-  },
-  {
-    name: 'Origins',
-    renderLink: 'https://origins-wtyd.onrender.com',
-    slug: 'origins',
-    image: OriginsImg,
-  },
-  {
-    name: 'Revelations',
-    renderLink: 'https://revelations-guide.onrender.com',
-    slug: 'revelations',
-    image: RevelationsImg,
-  },
-  {
-    name: 'Mob of the Dead',
-    renderLink: 'https://motd-guide.onrender.com',
-    slug: 'mob-of-the-dead',
-    image: MobOfTheDeadImg,
-  },
-  {
-    name: 'Der Eisendrache',
-    renderLink: 'https://der-eisendrache-guide.onrender.com',
-    slug: 'der-eisendrache',
-    image: DerEisendracheImg,
-  },
-  {
-    name: 'Gorod Krovi',
-    renderLink: 'https://gorod-krovi-guide.onrender.com',
-    slug: 'gorod-krovi',
-    image: GorodKroviImg,
-  },
-  {
-    name: 'Side Easter Eggs',
-    renderLink: '/side-easter-eggs',
-    slug: null,
-    image: SideEEImg,
-  },
+  { name: 'All Maps', renderLink: '/allmaps', slug: null, image: AllMapsImg },
+  { name: 'Origins', renderLink: '/maps/origins', slug: 'origins', image: OriginsImg },
+  { name: 'Revelations', renderLink: '/maps/revelations', slug: 'revelations', image: RevelationsImg },
+  { name: 'Mob of the Dead', renderLink: '/maps/mob-of-the-dead', slug: 'mob-of-the-dead', image: MobOfTheDeadImg },
+  { name: 'Der Eisendrache', renderLink: '/maps/der-eisendrache', slug: 'der-eisendrache', image: DerEisendracheImg },
+  { name: 'Gorod Krovi', renderLink: '/maps/gorod-krovi', slug: 'gorod-krovi', image: GorodKroviImg },
+  { name: 'Side Easter Eggs', renderLink: '/side-easter-eggs', slug: null, image: SideEEImg },
 ];
 
 export default featuredMaps;
