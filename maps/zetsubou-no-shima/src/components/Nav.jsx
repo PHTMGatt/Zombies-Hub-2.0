07a36c6c597c311526fab21f02f9fbc26e842d2a
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-01 23:43:07 -0400

    Namespace Zetsubou navigation under Hub routes

--- maps/zetsubou-no-shima/src/components/Nav.jsx
+++ maps/zetsubou-no-shima/src/components/Nav.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/Nav.css';
 
+const BASE = '/maps/zetsubou-no-shima';
+
 const Nav = ({ isOpen }) => {
   return (
     <nav className={`zets-nav ${isOpen ? 'open' : ''}`}>
-      <NavLink to="/" end>Overview</NavLink>
-      <NavLink to="/prerequisites">Prerequisites</NavLink>
-      <NavLink to="/reveal-blueprint">Reveal Blueprint</NavLink>
-      <NavLink to="/cogs">Cogs</NavLink>
-      <NavLink to="/elevator-battle">Boss Fight</NavLink>
-      <NavLink to="/buildables">Buildables</NavLink>
+      <NavLink to={BASE} end>Overview</NavLink>
+      <NavLink to={`${BASE}/prerequisites`}>Prerequisites</NavLink>
+      <NavLink to={`${BASE}/reveal-blueprint`}>Reveal Blueprint</NavLink>
+      <NavLink to={`${BASE}/cogs`}>Cogs</NavLink>
+      <NavLink to={`${BASE}/elevator-battle`}>Boss Fight</NavLink>
+      <NavLink to={`${BASE}/buildables`}>Buildables</NavLink>
     </nav>
   );
 };
