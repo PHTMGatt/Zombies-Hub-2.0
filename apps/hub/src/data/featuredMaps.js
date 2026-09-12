7681f50f1eb1bb686b4309514b0415adc40b75b4
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-12 15:45:44 -0400

    Replace legacy featured Render links with internal routes

--- apps/hub/src/data/featuredMaps.js
+++ apps/hub/src/data/featuredMaps.js
@@ -5,15 +5,16 @@ import MobOfTheDeadImg   from '../assets/images/Featured/MOTD.jpg';
 import DerEisendracheImg from '../assets/images/Featured/DE.webp';
 import GorodKroviImg     from '../assets/images/Featured/GK.jpg';
 import SideEEImg         from '../assets/images/Featured/SideEE.jpg';
+import { getDedicatedGuideRoute } from './dedicatedGuides';
 
 const featuredMaps = [
-  { name: 'All Maps', renderLink: '/allmaps', slug: null, image: AllMapsImg },
-  { name: 'Origins', renderLink: '/maps/origins', slug: 'origins', image: OriginsImg },
-  { name: 'Revelations', renderLink: '/maps/revelations', slug: 'revelations', image: RevelationsImg },
-  { name: 'Mob of the Dead', renderLink: '/maps/mob-of-the-dead', slug: 'mob-of-the-dead', image: MobOfTheDeadImg },
-  { name: 'Der Eisendrache', renderLink: '/maps/der-eisendrache', slug: 'der-eisendrache', image: DerEisendracheImg },
-  { name: 'Gorod Krovi', renderLink: '/maps/gorod-krovi', slug: 'gorod-krovi', image: GorodKroviImg },
-  { name: 'Side Easter Eggs', renderLink: '/side-easter-eggs', slug: null, image: SideEEImg },
+  { name: 'All Maps', route: '/allmaps', image: AllMapsImg },
+  { name: 'Origins', route: getDedicatedGuideRoute('origins'), image: OriginsImg },
+  { name: 'Revelations', route: getDedicatedGuideRoute('revelations'), image: RevelationsImg },
+  { name: 'Mob of the Dead', route: getDedicatedGuideRoute('mob-of-the-dead'), image: MobOfTheDeadImg },
+  { name: 'Der Eisendrache', route: getDedicatedGuideRoute('der-eisendrache'), image: DerEisendracheImg },
+  { name: 'Gorod Krovi', route: getDedicatedGuideRoute('gorod-krovi'), image: GorodKroviImg },
+  { name: 'Side Easter Eggs', route: '/side-easter-eggs', image: SideEEImg },
 ];
 
 export default featuredMaps;
