313dd119dacf39cb34915c0aa35c664d8c36713d
Bryen Gatt <PHTMGatt@Gmail.com>
2026-09-15 03:48:07 -0400

    Add safe fallbacks to dedicated guide routes

--- maps/revelations/routes/RevelationsRoutes.jsx
+++ maps/revelations/routes/RevelationsRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Header from '../src/components/Header';
 import Footer from '../src/components/Footer';
 import MainGuide from '../src/pages/MainGuide';
@@ -20,6 +20,7 @@ export default function RevelationsRoutes() {
         <Route path="egg-locations" element={<EggLocations />} />
         <Route path="summoning-key" element={<SummoningKey />} />
         <Route path="sound-step" element={<SoundStep />} />
+        <Route path="*" element={<Navigate to="/maps/revelations" replace />} />
       </Routes>
       <Footer />
     </section>
